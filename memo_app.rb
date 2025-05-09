cae6a3b724c750360001ccf775fb2fedfa881ec8
nakaj1st <0619nakaji@gmail.com>
2025-05-09 17:13:11 +0900

    メモの追加ページ、機能追加

--- memo_app.rb
+++ memo_app.rb
@@ -9,10 +9,29 @@ before do
 end
 
 get '/' do
-  erb :index
+  redirect '/memos'
+end
+
+get '/memos' do
+  @memos = JSON.parse(open('public/memo.json').read)
+  erb :memos
 end
 
 get '/memos/:id' do
   @memo = @memos[params[:id].to_i]
   erb :content
+end
+
+get '/new-memos' do
+  erb :new_memo
+end
+
+post '/memos' do
+  @title = params[:title]
+  @content = params[:content]
+  File.open('public/memo.json', 'w') do |file|
+    @memos[@memos.size] = { id: @memos.size.to_s, title: @title, content: @content }
+    JSON.dump(@memos, file)
+  end
+  redirect '/'
 end
