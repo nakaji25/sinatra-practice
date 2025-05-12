fb53ada27d40a243fe295200f308fe8be98851c9
nakaj1st <0619nakaji@gmail.com>
2025-05-12 18:58:41 +0900

    編集機能追加

--- memo_app.rb
+++ memo_app.rb
@@ -34,12 +34,26 @@ get '/new-memos' do
   erb :new_memo
 end
 
+get '/memos/:id/edit' do
+  @memos.each do |memo|
+    @memo = memo if memo['id'] == params[:id]
+  end
+  erb :edit
+end
+
 post '/memos' do
   @title = params[:title]
   @content = params[:content]
   @memos[@memos.size] = { id: @memos.size.to_s, title: @title, content: @content }
   save_memos(FILE_PATH)
   redirect '/memos'
+end
+
+patch '/memos/:id' do
+  @memos[params[:id].to_i]['title'] = params[:title]
+  @memos[params[:id].to_i]['content'] = params[:content]
+  save_memos(FILE_PATH)
+  redirect "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do
